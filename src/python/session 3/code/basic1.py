f2987612977977810ec1e2223a1be5abc63358ee
Fahd <fahdsherif2005@gmail.com>
2026-07-31 16:50:05 +0300

    add basic 3

---
+++ src/python/session 3/code/basic1.py
@@ -0,0 +1,21 @@
+'''
+this first basic module of python
+fun_1: get_info
+'''
+
+
+def get_info(name: str, age: int):
+    '''  function for person information get the name and age and
+        return back again to user
+
+    Args:
+        param_1: name get the name of person
+        type of param_1: str
+        param_2: age get the age from user
+        type_param_2: int 
+
+        return: this is function return the name and the age
+        type_return: ste, age
+
+    '''
+    return f"my name is {name}, age is {age}"
